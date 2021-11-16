d952166b6b0dee53958e97993e13a5e53dcbff16
wooy0ng <yygg9800@naver.com>
2021-11-17 01:15:51 +0900

    add script_01.js

---
+++ script/script_01.js
@@ -0,0 +1,81 @@
+
+function getPrevUrl(){
+    var url = window.location.href;
+    var dir = url.split('/');
+    dir.splice(0, 3);
+    var idx = dir.pop().split('.')[0];
+    if(idx == 'index'){
+        var result = "None";
+    }
+    else{
+        idx = Number(idx) - 1;
+        if(idx){
+            dir.push(String(idx) + '.html');
+            var result = dir.join('/');
+        } else{
+            var result = "None";
+        }
+    }
+    return result;
+}
+function getNextUrl(){
+    var url = window.location.href;
+    var dir = url.split('/');
+    dir.splice(0, 3);
+    var idx = dir.pop().split('.')[0];
+    if(idx == 'index'){
+        var result = "None";
+    }
+    else{
+        idx = Number(idx) + 1;
+        if(idx){
+            dir.push(String(idx) + '.html');
+            var result = dir.join('/');
+        } else{
+            var result = "None";
+        }
+    }
+    return result;
+}
+function checkFileExists(url){
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+
+    if(http.status != 404){
+        return true;
+    }
+    return false;
+}
+
+
+function goPrevUrl(){
+    var url = getPrevUrl();
+    if(url != 'None'){
+        location.href = url;
+    }
+    
+}
+
+function goNextUrl(){
+    var url = getNextUrl();
+    if(url != 'None'){
+        location.href = url;
+    }
+    else if(checkFileExists(url)){
+        location.href = url;
+    }
+}
+
+function getPrevUrlTitle(){
+    // To Do : dynamic title
+    const request = require('request'),
+        cheerio = require('cheerio');
+    var url = getPrevUrl();
+
+    request(url, function(err, res, html){
+        if(!err){
+            var $ = cheerio.load(html)  
+        }
+    })
+}
